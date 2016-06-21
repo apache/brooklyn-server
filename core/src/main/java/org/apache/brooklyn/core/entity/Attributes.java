5b3c412a229ab3b7f553148471989cf741d12a30
Valentin Aitken <bostko@gmail.com>
2016-06-21 18:10:31 +0300

    BROOKLYN-264: Stop app while VM still being provisioned
    - put a wait in MachineLifecycleEffectorTasks.java#doStop for MachineLocations
    - Live test for jclouds locations
    - integration test for byon locations

--- core/src/main/java/org/apache/brooklyn/core/entity/Attributes.java
+++ core/src/main/java/org/apache/brooklyn/core/entity/Attributes.java
@@ -144,7 +144,19 @@ public interface Attributes {
     
     AttributeSensor<URI> MAIN_URI = MainUri.MAIN_URI;
 
- // this class is added because the MAIN_URI relies on a static initialization which unfortunately can't be added to an interface.
+    AttributeSensor<JcloudsProvisioningState> JCLOUDS_PROVISIONING_RUNNING = new BasicAttributeSensor(TypeToken.of(JcloudsProvisioningState.class), "jclouds.provisioning.running",
+            "Internal sensor. Please do NOT change its value across entities.", AttributeSensor.SensorPersistenceMode.NONE);
+    /**
+     * Used only by {@link org.apache.brooklyn.entity.software.base.lifecycle.MachineLifecycleEffectorTasks#doStop}
+     */
+    @Beta
+    enum JcloudsProvisioningState {
+        STARTED,
+        DONE,
+        NONE
+    }
+
+    // this class is added because the MAIN_URI relies on a static initialization which unfortunately can't be added to an interface.
     class MainUri {
         private final static AttributeSensor<URI> MAIN_URI = Sensors.newSensor(URI.class, "main.uri", "Main URI for contacting the service/endpoint offered by this entity");
 
