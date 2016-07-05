ae5659aa3600887e5445fc480e183a45f51687f9
Geoff Macartney <geoff.macartney@cloudsoftcorp.com>
2016-07-05 15:03:25 +0100

    Fix etcd connection failure for different external and subnet networks.
    Builds on https://github.com/cloudsoft/container-service/pull/55,
    do not merge until after that.
    
    Adds subnet address as as subjectAltName as well as host address.
    
    This is because on Softlayer or other clouds where the public
    address is on a different network from the subnet address,
    swarm fails to connect to the etcd discovery mechanism.
    
    The problem is that the public host address is configured in the
    certificate but the etcd config is using the subnet address for the
    client and peer urls. When swarm nodes connect on the subnet
    address the certificate presented does not match the IP address
    requested and the connection fails.
    
    By adding the subnet address as another alternative name it
    is possible to connect using either address.

--- getcert.sh
+++ getcert.sh
@@ -11,15 +11,5 @@ set -e
 mkdir -p ${CERT_DIR}
 curl -L ${CA}/cacert/ca.pem --output ${CERT_DIR}/ca.pem
 openssl genrsa -out ${CERT_DIR}/key.pem 2048
-openssl req  -new -key ${CERT_DIR}/key.pem -days 1825 -out ${CERT_DIR}/csr.pem <<-EOF
-.
-.
-.
-.
-.
-*
-.
-.
-.
-EOF
+openssl req  -new -key ${CERT_DIR}/key.pem -days 1825 -out ${CERT_DIR}/csr.pem -subj "/CN=$(hostname)"
 curl -X POST --data-binary @${CERT_DIR}/csr.pem ${CA}/sign > ${CERT_DIR}/cert.pem
