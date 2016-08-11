724458a7e438f23e6fecb1da34b5f1598edf8faf
Andrew Donald Kennedy <andrew.kennedy@cloudsoftcorp.com>
2016-08-11 23:56:51 +0100

    Add ServiceStateLogic enricher for better child state propagation

--- core/src/main/java/org/apache/brooklyn/entity/stock/ConditionalEntityImpl.java
+++ core/src/main/java/org/apache/brooklyn/entity/stock/ConditionalEntityImpl.java
@@ -33,10 +33,12 @@ import org.apache.brooklyn.core.entity.BrooklynConfigKeys;
 import org.apache.brooklyn.core.entity.Entities;
 import org.apache.brooklyn.core.entity.lifecycle.Lifecycle;
 import org.apache.brooklyn.core.entity.lifecycle.ServiceStateLogic;
+import org.apache.brooklyn.core.entity.lifecycle.ServiceStateLogic.ComputeServiceIndicatorsFromChildrenAndMembers;
 import org.apache.brooklyn.core.entity.trait.Startable;
 import org.apache.brooklyn.core.location.Locations;
 import org.apache.brooklyn.enricher.stock.Enrichers;
 import org.apache.brooklyn.util.collections.MutableSet;
+import org.apache.brooklyn.util.collections.QuorumCheck;
 import org.apache.brooklyn.util.exceptions.Exceptions;
 import org.apache.brooklyn.util.time.Duration;
 
@@ -72,8 +74,11 @@ public class ConditionalEntityImpl extends BasicStartableImpl implements Conditi
                 child = addChild(EntitySpec.create(spec));
                 sensors().set(CONDITIONAL_ENTITY, child);
 
-                // Add enrichers for sensor propagateion
+                // Add enrichers for sensor propagation
                 enrichers().add(Enrichers.builder().propagating(Startable.SERVICE_UP).from(child).build());
+                enrichers().add(ServiceStateLogic.newEnricherFromChildrenState()
+                        .suppressDuplicates(true)
+                        .configure(ComputeServiceIndicatorsFromChildrenAndMembers.RUNNING_QUORUM_CHECK, QuorumCheck.QuorumChecks.all()));
                 if (Boolean.TRUE.equals(propagate)) {
                     if (sensors.isEmpty()) {
                         enrichers().add(Enrichers.builder().propagatingAllButUsualAnd().from(child).build());
