136dd111e681ee50f13adc7435df84f635337cf2
Sam Corbett <sam.corbett@cloudsoftcorp.com>
2016-09-22 11:31:38 +0100

    Move usage listener tests to core from software-base

--- core/src/test/java/org/apache/brooklyn/core/mgmt/usage/RecordingUsageListener.java
+++ core/src/test/java/org/apache/brooklyn/core/mgmt/usage/RecordingUsageListener.java
@@ -16,7 +16,8 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.brooklyn.entity.software.base.test.core.mgmt.usage;
+
+package org.apache.brooklyn.core.mgmt.usage;
 
 import java.util.List;
 
