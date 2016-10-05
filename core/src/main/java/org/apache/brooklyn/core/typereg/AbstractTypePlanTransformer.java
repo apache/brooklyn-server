3f986ec9c5340bd1f4dbef9cbdf5e26a27e1f3d7
Geoff Macartney <geoff.macartney@cloudsoftcorp.com>
2016-10-05 13:40:33 +0100

    catalogItemIdIfNotNull is no longer used

--- core/src/main/java/org/apache/brooklyn/core/typereg/AbstractTypePlanTransformer.java
+++ core/src/main/java/org/apache/brooklyn/core/typereg/AbstractTypePlanTransformer.java
@@ -103,7 +103,6 @@ public abstract class AbstractTypePlanTransformer implements BrooklynTypePlanTra
                 @Override protected Object visitSpec() {
                     try { 
                         AbstractBrooklynObjectSpec<?, ?> result = createSpec(type, context);
-                        // see notes on catalogItemIdIfNotNull
                         result.nestCatalogItemId(type.getId());
                         return result;
                     } catch (Exception e) { throw Exceptions.propagate(e); }
