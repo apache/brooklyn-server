7c4c6f9dc56f09a7c79b094052ef3e9463e1610d
Aled Sage <aled.sage@gmail.com>
2016-11-24 22:19:12 +0000

    openshift live tests: extend Kubernetes tests

--- openshift-location/src/test/java/io/cloudsoft/amp/container/openshift/location/OpenShiftLocationLiveTest.java
+++ openshift-location/src/test/java/io/cloudsoft/amp/container/openshift/location/OpenShiftLocationLiveTest.java
@@ -1,87 +1,32 @@
 package io.cloudsoft.amp.container.openshift.location;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
-import static org.testng.Assert.assertTrue;
-
-import java.util.List;
 import java.util.Map;
 
-import org.apache.brooklyn.api.entity.EntitySpec;
-import org.apache.brooklyn.api.location.MachineDetails;
-import org.apache.brooklyn.api.location.MachineLocation;
-import org.apache.brooklyn.api.location.OsDetails;
-import org.apache.brooklyn.api.location.PortRange;
-import org.apache.brooklyn.api.sensor.AttributeSensor;
-import org.apache.brooklyn.api.sensor.EnricherSpec;
-import org.apache.brooklyn.core.config.ConfigKeys;
-import org.apache.brooklyn.core.entity.EntityAsserts;
-import org.apache.brooklyn.core.location.BasicMachineDetails;
-import org.apache.brooklyn.core.location.LocationConfigKeys;
-import org.apache.brooklyn.core.location.access.PortForwardManager;
-import org.apache.brooklyn.core.location.access.PortForwardManagerLocationResolver;
-import org.apache.brooklyn.core.network.OnPublicNetworkEnricher;
-import org.apache.brooklyn.core.objs.BasicSpecParameter;
-import org.apache.brooklyn.core.sensor.PortAttributeSensorAndConfigKey;
-import org.apache.brooklyn.core.sensor.Sensors;
-import org.apache.brooklyn.core.test.BrooklynAppLiveTestSupport;
-import org.apache.brooklyn.entity.software.base.VanillaSoftwareProcess;
-import org.apache.brooklyn.location.ssh.SshMachineLocation;
 import org.apache.brooklyn.util.collections.MutableMap;
-import org.apache.brooklyn.util.net.Networking;
 import org.apache.brooklyn.util.os.Os;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Lists;
-import com.google.common.net.HostAndPort;
-
-import io.cloudsoft.amp.container.kubernetes.location.KubernetesLocationConfig;
+import io.cloudsoft.amp.container.kubernetes.location.KubernetesLocationLiveTest;
 
 /**
- * Assumes that a pre-existing OpenShift endpoint is available. See system properties and the defaults below.
+ * Tests deploying containers via the {@code openshift"} location, to an OpenShift endpoint. 
+ * By extending {@link KubernetesLocationLiveTest}, we get all the k8s tests.
  */
-public class OpenShiftLocationLiveTest extends BrooklynAppLiveTestSupport {
+public class OpenShiftLocationLiveTest extends KubernetesLocationLiveTest {
 
+    @SuppressWarnings("unused")
     private static final Logger LOG = LoggerFactory.getLogger(OpenShiftLocationLiveTest.class);
 
-    private static final String OPENSHIFT_ENDPOINT = System.getProperty("test.amp.openshift.endpoint", "https://192.168.99.100:8443/");
-    private static final String CERTS_BASE_DIR = System.getProperty("test.amp.openshift.certsBaseDir", Os.mergePaths(System.getProperty("user.home"), "openshift-certs"));
-    private static final String CA_CERT_FILE = System.getProperty("test.amp.openshift.caCert", Os.mergePaths(CERTS_BASE_DIR, "ca.crt"));
-    private static final String CLIENT_CERT_FILE = System.getProperty("test.amp.openshift.clientCert", Os.mergePaths(CERTS_BASE_DIR, "admin.crt"));
-    private static final String CLIENT_KEY_FILE = System.getProperty("test.amp.openshift.clientKey", Os.mergePaths(CERTS_BASE_DIR, "admin.key"));
-    private static final String NAMESPACE = System.getProperty("test.amp.openshift.namespace", "");
+    public static final String OPENSHIFT_ENDPOINT = System.getProperty("test.amp.openshift.endpoint", "https://192.168.99.100:8443/");
+    public static final String CERTS_BASE_DIR = System.getProperty("test.amp.openshift.certsBaseDir", Os.mergePaths(System.getProperty("user.home"), "openshift-certs"));
+    public static final String CA_CERT_FILE = System.getProperty("test.amp.openshift.caCert", Os.mergePaths(CERTS_BASE_DIR, "ca.crt"));
+    public static final String CLIENT_CERT_FILE = System.getProperty("test.amp.openshift.clientCert", Os.mergePaths(CERTS_BASE_DIR, "admin.crt"));
+    public static final String CLIENT_KEY_FILE = System.getProperty("test.amp.openshift.clientKey", Os.mergePaths(CERTS_BASE_DIR, "admin.key"));
+    public static final String NAMESPACE = System.getProperty("test.amp.openshift.namespace", "");
 
-    protected OpenShiftLocation loc;
-    protected List<MachineLocation> machines;
-
-    @BeforeMethod(alwaysRun=true)
     @Override
-    public void setUp() throws Exception {
-        super.setUp();
-        machines = Lists.newCopyOnWriteArrayList();
-    }
-
-    // FIXME: Clear up properly: Test leaves deployment, replicas and pods behind if obtain fails.
-    @AfterMethod(alwaysRun=true)
-    @Override
-    public void tearDown() throws Exception {
-        for (MachineLocation machine : machines) {
-            try {
-                loc.release(machine);
-            } catch (Exception e) {
-                LOG.error("Error releasing machine "+machine+" in location "+loc, e);
-            }
-        }
-        super.tearDown();
-    }
-
-    protected OpenShiftLocation newOpenShiftLocation(Map<String, ?> flags) throws Exception {
+    protected OpenShiftLocation newKubernetesLocation(Map<String, ?> flags) throws Exception {
         Map<String,?> allFlags = MutableMap.<String,Object>builder()
                 .put("endpoint", OPENSHIFT_ENDPOINT)
                 .put("kubernetes.caCert", CA_CERT_FILE)
@@ -92,102 +37,5 @@ public class OpenShiftLocationLiveTest extends BrooklynAppLiveTestSupport {
                 .putAll(flags)
                 .build();
         return (OpenShiftLocation) mgmt.getLocationRegistry().getLocationManaged("openshift", allFlags);
-    }
-
-    // TODO Fails because can't ssh to container
-    @Test(groups={"Live", "Broken"}, enabled=false)
-    public void testDefaultImage() throws Exception {
-        loc = newOpenShiftLocation(ImmutableMap.<String, Object>of());
-        SshMachineLocation machine = newContainerMachine(loc, ImmutableMap.<String, Object>builder()
-                .put(LocationConfigKeys.CALLER_CONTEXT.getName(), app)
-                .build());
-
-        assertOsNameContains(machine, "ubuntu");
-    }
-
-    @Test(groups={"Live"})
-    public void testCloudsoftCentosImage() throws Exception {
-        loc = newOpenShiftLocation(ImmutableMap.<String, Object>of());
-        SshMachineLocation machine = newContainerMachine(loc, ImmutableMap.<String, Object>builder()
-                .put(KubernetesLocationConfig.IMAGE.getName(), "cloudsoft/centos:7")
-                .put(KubernetesLocationConfig.LOGIN_USER_PASSWORD.getName(), "p4ssw0rd")
-                .put(LocationConfigKeys.CALLER_CONTEXT.getName(), app)
-                .build());
-
-        assertTrue(machine.isSshable(), "not sshable machine="+machine);
-        assertOsNameContains(machine, "centos");
-    }
-
-    @Test(groups={"Live"})
-    public void testOpenPorts() throws Exception {
-        List<Integer> inboundPorts = ImmutableList.of(22, 443, 8000, 8081);
-        loc = newOpenShiftLocation(ImmutableMap.<String, Object>of());
-        SshMachineLocation machine = newContainerMachine(loc, ImmutableMap.<String, Object>builder()
-                .put(KubernetesLocationConfig.IMAGE.getName(), "cloudsoft/centos:7")
-                .put(KubernetesLocationConfig.LOGIN_USER_PASSWORD.getName(), "p4ssw0rd")
-                .put(KubernetesLocationConfig.INBOUND_PORTS.getName(), inboundPorts)
-                .put(LocationConfigKeys.CALLER_CONTEXT.getName(), app)
-                .build());
-        assertTrue(machine.isSshable());
-        
-        String publicHostText = machine.getSshHostAndPort().getHostText();
-        PortForwardManager pfm = (PortForwardManager) mgmt.getLocationRegistry().getLocationManaged(PortForwardManagerLocationResolver.PFM_GLOBAL_SPEC);
-        for (int targetPort : inboundPorts) {
-            HostAndPort mappedPort = pfm.lookup(machine, targetPort);
-            assertNotNull(mappedPort, "no mapping for targetPort "+targetPort);
-            assertEquals(mappedPort.getHostText(), publicHostText);
-            assertTrue(mappedPort.hasPort(), "no port-part in "+mappedPort+" for targetPort "+targetPort);
-        }
-    }
-
-    @Test(groups={"Live"})
-    public void testDeployNetcatApp() throws Exception {
-        loc = newOpenShiftLocation(ImmutableMap.of(
-                KubernetesLocationConfig.IMAGE.getName(), "cloudsoft/centos:7",
-                KubernetesLocationConfig.LOGIN_USER_PASSWORD.getName(), "p4ssw0rd"));
-        
-        PortAttributeSensorAndConfigKey portKey = ConfigKeys.newPortSensorAndConfigKey("netcat.port", "my port");
-        AttributeSensor<String> publicPortSensor = Sensors.newStringSensor("netcat.endpoint.mapped.public");
-        
-        // Setting port with BasicSpecParameter so it is picked up as a port that must be opened
-        // Not using `sudo`, because already running as root
-        VanillaSoftwareProcess entity = app.addChild(EntitySpec.create(VanillaSoftwareProcess.class)
-                .parametersAdd(ImmutableList.of(new BasicSpecParameter<PortRange>(portKey.getName(), true, portKey.getConfigKey(), portKey)))
-                .configure(VanillaSoftwareProcess.INSTALL_COMMAND,
-                        "yum install -y nc")
-                .configure(VanillaSoftwareProcess.LAUNCH_COMMAND,
-                        "echo $MESSAGE | nc -l $NETCAT_PORT &" + "\n" +
-                        "echo $! > $PID_FILE")
-                .configure(VanillaSoftwareProcess.CHECK_RUNNING_COMMAND, 
-                        "test -f $PID_FILE && ps -p $(cat $PID_FILE)") 
-                .configure(VanillaSoftwareProcess.SHELL_ENVIRONMENT.getName(), ImmutableMap.of( 
-                        "MESSAGE", "mymessage",
-                        "NETCAT_PORT", "8081"))
-                .configure(portKey.getName(), 8081)
-                .enricher(EnricherSpec.create(OnPublicNetworkEnricher.class)
-                        .configure(OnPublicNetworkEnricher.SENSORS, ImmutableList.of(portKey)))
-                );
-        
-        app.start(ImmutableList.of(loc));
-        
-        String publicMapped = EntityAsserts.assertAttributeEventuallyNonNull(entity, publicPortSensor);
-        HostAndPort publicPort = HostAndPort.fromString(publicMapped);
-        
-        assertTrue(Networking.isReachable(publicPort), "publicPort="+publicPort);
-    }
-
-    protected void assertOsNameContains(SshMachineLocation machine, String expectedNamePart) {
-        MachineDetails machineDetails = app.getExecutionContext()
-                .submit(BasicMachineDetails.taskForSshMachineLocation(machine))
-                .getUnchecked();
-        OsDetails osDetails = machineDetails.getOsDetails();
-        String osName = osDetails.getName();
-        assertTrue(osName != null && osName.toLowerCase().contains(expectedNamePart), "osDetails="+osDetails);
-    }
-    
-    protected SshMachineLocation newContainerMachine(OpenShiftLocation loc, Map<?, ?> flags) throws Exception {
-        MachineLocation result = loc.obtain(flags);
-        machines.add(result);
-        return (SshMachineLocation) result;
     }
 }
---
+++ openshift-location/src/test/java/io/cloudsoft/amp/container/openshift/location/OpenShiftLocationYamlLiveTest.java
@@ -0,0 +1,67 @@
+package io.cloudsoft.amp.container.openshift.location;
+
+import static io.cloudsoft.amp.container.openshift.location.OpenShiftLocationLiveTest.CA_CERT_FILE;
+import static io.cloudsoft.amp.container.openshift.location.OpenShiftLocationLiveTest.CLIENT_CERT_FILE;
+import static io.cloudsoft.amp.container.openshift.location.OpenShiftLocationLiveTest.CLIENT_KEY_FILE;
+import static io.cloudsoft.amp.container.openshift.location.OpenShiftLocationLiveTest.NAMESPACE;
+import static io.cloudsoft.amp.container.openshift.location.OpenShiftLocationLiveTest.OPENSHIFT_ENDPOINT;
+
+import org.testng.annotations.BeforeMethod;
+
+import com.google.common.base.Joiner;
+
+import io.cloudsoft.amp.container.kubernetes.location.KubernetesLocationYamlLiveTest;
+
+/**
+ * Tests YAML apps via the {@code openshift"} location, to an OpenShift endpoint.
+ * By extending {@link KubernetesLocationYamlLiveTest}, we get all the k8s tests.
+ * 
+ * It needs configured with something like:
+ * 
+ * <pre>
+ * {@code
+ * -Dtest.amp.openshift.endpoint=https://master.example.com:8443/
+ * -Dtest.amp.openshift.certsBaseDir=/Users/aled/repos/grkvlt/40bdf09b09d5896e19a9d287f41d39bb
+ * -Dtest.amp.openshift.namespace=test
+ * }
+ * </pre>
+ */
+public class OpenShiftLocationYamlLiveTest extends KubernetesLocationYamlLiveTest {
+
+    // TODO testTomcatContainer seems flaky on the OpenShift deployed at 10.101.1.139, 
+    // when using node2.
+    //
+    // The container's log shows it takes 355 seconds to deploy the default web application 
+    // directory /usr/local/tomcat/webapps/ROOT:
+    //    24-Nov-2016 22:04:11.906 INFO [main] org.apache.catalina.core.StandardEngine.startInternal Starting Servlet Engine: Apache Tomcat/8.0.39
+    //    24-Nov-2016 22:04:11.940 INFO [localhost-startStop-1] org.apache.catalina.startup.HostConfig.deployDirectory Deploying web application directory /usr/local/tomcat/webapps/ROOT
+    //    24-Nov-2016 22:10:07.093 INFO [localhost-startStop-1] org.apache.catalina.util.SessionIdGeneratorBase.createSecureRandom Creation of SecureRandom instance for session ID generation using [SHA1PRNG] took [354,156] milliseconds.
+    //    24-Nov-2016 22:10:07.123 INFO [localhost-startStop-1] org.apache.catalina.startup.HostConfig.deployDirectory Deployment of web application directory /usr/local/tomcat/webapps/ROOT has finished in 355,183 ms
+    //    24-Nov-2016 22:10:07.133 INFO [localhost-startStop-1] org.apache.catalina.startup.HostConfig.deployDirectory Deploying web application directory /usr/local/tomcat/webapps/docs
+    //    24-Nov-2016 22:10:07.172 INFO [localhost-startStop-1] org.apache.catalina.startup.HostConfig.deployDirectory Deployment of web application directory /usr/local/tomcat/webapps/docs has finished in 40 ms
+    //    24-Nov-2016 22:10:07.173 INFO [localhost-startStop-1] org.apache.catalina.startup.HostConfig.deployDirectory Deploying web application directory /usr/local/tomcat/webapps/examples
+    //    24-Nov-2016 22:10:08.051 INFO [localhost-startStop-1] org.apache.catalina.startup.HostConfig.deployDirectory Deployment of web application directory /usr/local/tomcat/webapps/examples has finished in 878 ms
+    //    24-Nov-2016 22:10:08.052 INFO [localhost-startStop-1] org.apache.catalina.startup.HostConfig.deployDirectory Deploying web application directory /usr/local/tomcat/webapps/host-manager
+    //    24-Nov-2016 22:10:08.104 INFO [localhost-startStop-1] org.apache.catalina.startup.HostConfig.deployDirectory Deployment of web application directory /usr/local/tomcat/webapps/host-manager has finished in 52 ms
+    //    24-Nov-2016 22:10:08.104 INFO [localhost-startStop-1] org.apache.catalina.startup.HostConfig.deployDirectory Deploying web application directory /usr/local/tomcat/webapps/manager
+    //    24-Nov-2016 22:10:08.159 INFO [localhost-startStop-1] org.apache.catalina.startup.HostConfig.deployDirectory Deployment of web application directory /usr/local/tomcat/webapps/manager has finished in 55 ms
+    //
+    // With node1, it takes only 6 seconds to deploy the we app.
+
+    @BeforeMethod(alwaysRun=true)
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        
+        locationYaml = Joiner.on("\n").join(
+                "location:",
+                "  openshift:",
+                "    " + OpenShiftLocation.CLOUD_ENDPOINT.getName() + ": \"" + OPENSHIFT_ENDPOINT + "\"",
+                "    " + OpenShiftLocation.CA_CERT.getName() + ": \"" + CA_CERT_FILE + "\"",
+                "    " + OpenShiftLocation.CLIENT_CERT.getName() + ": \"" + CLIENT_CERT_FILE + "\"",
+                "    " + OpenShiftLocation.CLIENT_KEY.getName() + ": \"" + CLIENT_KEY_FILE + "\"",
+                "    " + OpenShiftLocation.NAMESPACE.getName() + ": \"" + NAMESPACE + "\"",
+                "    kubernetes.privileged: true",
+                "    " + OpenShiftLocation.LOGIN_USER_PASSWORD.getName() + ": p4ssw0rd");
+    }
+}
