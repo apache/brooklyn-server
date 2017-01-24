5672ded1cff0495dce2cd4ddb1984019b93586b7
Andrew Donald Kennedy <andrew.kennedy@cloudsoftcorp.com>
2017-01-24 05:30:52 +0000

    Disable SSH for KubernetesResource entity

--- kubernetes-location/src/main/java/io/cloudsoft/amp/containerservice/kubernetes/entity/KubernetesResourceImpl.java
+++ kubernetes-location/src/main/java/io/cloudsoft/amp/containerservice/kubernetes/entity/KubernetesResourceImpl.java
@@ -1,7 +1,17 @@
 package io.cloudsoft.amp.containerservice.kubernetes.entity;
 
+import org.apache.brooklyn.core.entity.BrooklynConfigKeys;
 import org.apache.brooklyn.entity.software.base.EmptySoftwareProcessImpl;
 
 public class KubernetesResourceImpl extends EmptySoftwareProcessImpl implements KubernetesResource {
 
+    @Override
+    public void init() {
+       super.init();
+
+      config().set(BrooklynConfigKeys.SKIP_ON_BOX_BASE_DIR_RESOLUTION, true);
+      config().set(PROVISIONING_PROPERTIES.subKey("useJcloudsSshInit"), false);
+      config().set(PROVISIONING_PROPERTIES.subKey("waitForSshable"), false);
+      config().set(EmptySoftwareProcessImpl.USE_SSH_MONITORING, false);
+    }
 }
