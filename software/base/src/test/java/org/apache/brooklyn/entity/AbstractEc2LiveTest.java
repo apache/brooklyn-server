a49a7dba55b7670ea993a5704f05c44cd93706c7
Geoff Macartney <geoff.macartney@cloudsoftcorp.com>
2017-01-26 14:10:59 +0000

    Add Openstack live test.
    A bit of refactoring to add openstack live test. There is some
    work required to tidy and rationalize the various multi platform
    live tests; this isn't it but may be a first step.

--- software/base/src/test/java/org/apache/brooklyn/entity/AbstractEc2LiveTest.java
+++ software/base/src/test/java/org/apache/brooklyn/entity/AbstractEc2LiveTest.java
@@ -48,7 +48,7 @@ import com.google.common.collect.ImmutableMap;
 /**
  * Runs a test with many different distros and versions.
  */
-public abstract class AbstractEc2LiveTest extends BrooklynAppLiveTestSupport {
+public abstract class AbstractEc2LiveTest extends AbstractMultiDistroLiveTest {
     
     // FIXME Currently have just focused on test_Debian_6; need to test the others as well!
 
@@ -60,6 +60,16 @@ public abstract class AbstractEc2LiveTest extends BrooklynAppLiveTestSupport {
     
     private static final Logger LOG = LoggerFactory.getLogger(AbstractEc2LiveTest.class);
 
+    @Override
+    public String getProvider() {
+        return PROVIDER;
+    }
+
+    @Override
+    public String getLocationSpec() {
+        return LOCATION_SPEC;
+    }
+
     public static final String PROVIDER = "aws-ec2";
     public static final String REGION_NAME = "us-east-1";
     public static final String LOCATION_SPEC = PROVIDER + (REGION_NAME == null ? "" : ":" + REGION_NAME);
@@ -70,25 +80,6 @@ public abstract class AbstractEc2LiveTest extends BrooklynAppLiveTestSupport {
     
     protected Location jcloudsLocation;
     
-    @BeforeMethod(alwaysRun=true)
-    @Override
-    public void setUp() throws Exception {
-        // Don't let any defaults from brooklyn.properties (except credentials) interfere with test
-        brooklynProperties = BrooklynProperties.Factory.newDefault();
-        brooklynProperties.remove("brooklyn.jclouds."+PROVIDER+".image-description-regex");
-        brooklynProperties.remove("brooklyn.jclouds."+PROVIDER+".image-name-regex");
-        brooklynProperties.remove("brooklyn.jclouds."+PROVIDER+".image-id");
-        brooklynProperties.remove("brooklyn.jclouds."+PROVIDER+".inboundPorts");
-        brooklynProperties.remove("brooklyn.jclouds."+PROVIDER+".hardware-id");
-
-        // Also removes scriptHeader (e.g. if doing `. ~/.bashrc` and `. ~/.profile`, then that can cause "stdin: is not a tty")
-        brooklynProperties.remove("brooklyn.ssh.config.scriptHeader");
-        
-        mgmt = new LocalManagementContextForTests(brooklynProperties);
-        
-        super.setUp();
-    }
-
     // Image ids for Debian: https://wiki.debian.org/Cloud/AmazonEC2Image/Squeeze
     @Test(groups = {"Live"})
     public void test_Debian_6() throws Exception {
@@ -147,47 +138,5 @@ public abstract class AbstractEc2LiveTest extends BrooklynAppLiveTestSupport {
     public void test_Suse_11sp3() throws Exception {
         // Image: {id=us-east-1/ami-c08fcba8, providerId=ami-c08fcba8, name=suse-sles-11-sp3-v20150127-pv-ssd-x86_64, location={scope=REGION, id=us-east-1, description=us-east-1, parent=aws-ec2, iso3166Codes=[US-VA]}, os={family=suse, arch=paravirtual, version=, description=amazon/suse-sles-11-sp3-v20150127-pv-ssd-x86_64, is64Bit=true}, description=SUSE Linux Enterprise Server 11 Service Pack 3 (PV, 64-bit, SSD-Backed), version=x86_64, status=AVAILABLE[available], loginUser=root, userMetadata={owner=013907871322, rootDeviceType=ebs, virtualizationType=paravirtual, hypervisor=xen}}
         runTest(ImmutableMap.of("imageId", "us-east-1/ami-c08fcba8", "hardwareId", SMALL_HARDWARE_ID, "loginUser", "ec2-user"));//, JcloudsLocationConfig.OPEN_IPTABLES.getName(), "true"));
-    }
-    
-    protected void runTest(Map<String,?> flags) throws Exception {
-        Map<String,?> allFlags = MutableMap.<String,Object>builder()
-                .put("tags", ImmutableList.of(getClass().getName()))
-                .putAll(flags)
-                .build();
-        jcloudsLocation = mgmt.getLocationRegistry().getLocationManaged(LOCATION_SPEC, allFlags);
-
-        doTest(jcloudsLocation);
-    }
-    
-    protected abstract void doTest(Location loc) throws Exception;
-    
-    protected void assertExecSsh(SoftwareProcess entity, List<String> commands) {
-        SshMachineLocation machine = Machines.findUniqueMachineLocation(entity.getLocations(), SshMachineLocation.class).get();
-        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-        ByteArrayOutputStream errStream = new ByteArrayOutputStream();
-        int result = machine.execScript(ImmutableMap.of("out", outStream, "err", errStream), "url-reachable", commands);
-        String out = new String(outStream.toByteArray());
-        String err = new String(errStream.toByteArray());
-        if (result == 0) {
-            LOG.debug("Successfully executed: cmds="+commands+"; stderr="+err+"; out="+out);
-        } else {
-            fail("Failed to execute: result="+result+"; cmds="+commands+"; stderr="+err+"; out="+out);
-        }
-    }
-    
-    protected void assertViaSshLocalPortListeningEventually(final SoftwareProcess server, final int port) {
-        Asserts.succeedsEventually(ImmutableMap.of("timeout", Duration.FIVE_MINUTES), new Runnable() {
-            @Override
-            public void run() {
-                assertExecSsh(server, ImmutableList.of("netstat -antp", "netstat -antp | grep LISTEN | grep "+port));
-            }});
-    }
-    
-    protected void assertViaSshLocalUrlListeningEventually(final SoftwareProcess server, final String url) {
-        Asserts.succeedsEventually(ImmutableMap.of("timeout", Duration.FIVE_MINUTES), new Runnable() {
-            @Override
-            public void run() {
-                assertExecSsh(server, ImmutableList.of(BashCommands.installPackage("curl"), "netstat -antp", "curl -k --retry 3 "+url));
-            }});
     }
 }
