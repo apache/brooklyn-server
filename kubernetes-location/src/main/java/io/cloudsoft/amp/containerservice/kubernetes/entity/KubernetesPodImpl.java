272e25a1e9ffb422193ca43de186c64ec4e19fa1
Andrew Donald Kennedy <andrew.kennedy@cloudsoftcorp.com>
2017-02-03 04:52:34 +0000

    Filter out empty KubernetesEmptyMachineLocation for KubernetesPod compatibility

--- kubernetes-location/src/main/java/io/cloudsoft/amp/containerservice/kubernetes/entity/KubernetesPodImpl.java
+++ kubernetes-location/src/main/java/io/cloudsoft/amp/containerservice/kubernetes/entity/KubernetesPodImpl.java
@@ -1,6 +1,20 @@
 package io.cloudsoft.amp.containerservice.kubernetes.entity;
 
+import static com.google.common.base.Predicates.and;
+import static com.google.common.base.Predicates.instanceOf;
+import static com.google.common.base.Predicates.not;
+import static org.apache.brooklyn.core.location.LocationPredicates.configEqualTo;
+
+import java.util.Collection;
+
+import org.apache.brooklyn.api.location.Location;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+
 import io.cloudsoft.amp.containerservice.dockercontainer.DockerContainerImpl;
+import io.cloudsoft.amp.containerservice.kubernetes.location.machine.KubernetesEmptyMachineLocation;
+import io.cloudsoft.amp.containerservice.kubernetes.location.machine.KubernetesMachineLocation;
 
 public class KubernetesPodImpl extends DockerContainerImpl implements KubernetesPod {
 
@@ -9,8 +23,17 @@ public class KubernetesPodImpl extends DockerContainerImpl implements Kubernetes
         super.init();
 
         if (config().get(IMAGE_NAME) == null && config().get(INBOUND_TCP_PORTS) == null) {
-            config().set(CHILDREN_STARTABLE_MODE, ChildStartableMode.BACKGROUND);
+            config().set(CHILDREN_STARTABLE_MODE, ChildStartableMode.BACKGROUND_LATE);
         }
+    }
+
+    // FIXME I hate myself...
+    @Override
+    public Collection<Location> getLocations() {
+        // TODO Remove this filtering once backwards compatible KubernetesPod usage is deprecated
+        return ImmutableSet.copyOf(Iterables.filter(super.getLocations(),
+                not(and(instanceOf(KubernetesEmptyMachineLocation.class),
+                        configEqualTo(KubernetesMachineLocation.KUBERNETES_RESOURCE_TYPE, KubernetesPod.EMPTY)))));
     }
 
 }
