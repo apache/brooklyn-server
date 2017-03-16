ad73fcf99676b4c06e30ac6f1ed1093225087f05
Aled Sage <aled.sage@gmail.com>
2017-03-16 00:04:03 +0000

    Fix classRenames for SshPollConfig anonymous classes

--- core/src/main/java/org/apache/brooklyn/feed/ssh/SshPollConfig.java
+++ core/src/main/java/org/apache/brooklyn/feed/ssh/SshPollConfig.java
@@ -18,8 +18,16 @@
  */
 package org.apache.brooklyn.feed.ssh;
 
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
 import org.apache.brooklyn.api.sensor.AttributeSensor;
 import org.apache.brooklyn.feed.CommandPollConfig;
+import org.apache.brooklyn.util.collections.MutableMap;
+
+import com.google.common.base.Predicate;
+import com.google.common.base.Supplier;
 
 /**
  * @deprecated since 0.11.0. Please use {@link CommandPollConfig}.
@@ -37,5 +45,40 @@ public class SshPollConfig<T> extends CommandPollConfig<T> {
     @Override
     public SshPollConfig<T> self() {
         return this;
+    }
+    
+    // TODO Kept in case it's persisted; new code will not use this.
+    @SuppressWarnings("unused")
+    private static final Predicate<SshPollValue> unused_DEFAULT_SUCCESS = new Predicate<SshPollValue>() {
+        @Override
+        public boolean apply(@Nullable SshPollValue input) {
+            return input != null && input.getExitStatus() == 0;
+        }};
+        
+    // TODO Kept in case it's persisted; new code will not use this.
+    // Can't just use deserializingClassRenames.properties, because persistence format
+    // for a static versus anonymous (non-static) inner class is different. 
+    @SuppressWarnings("unused")
+    private void unused_getEnvSupplier() {
+        new Supplier<Map<String,String>>() {
+            @Override
+            public Map<String, String> get() {
+                Map<String,String> result = MutableMap.of();
+                for (Supplier<Map<String, String>> envS: dynamicEnvironmentSupplier) {
+                    if (envS!=null) {
+                        Map<String, String> envM = envS.get();
+                        if (envM!=null) {
+                            mergeEnvMaps(envM, result);
+                        }
+                    }
+                }
+                return result;
+            }
+            private void mergeEnvMaps(Map<String,String> supplied, Map<String,String> target) {
+                if (supplied==null) return;
+                // as the value is a string there is no need to look at deep merge behaviour
+                target.putAll(supplied);
+            }
+        };
     }
 }
