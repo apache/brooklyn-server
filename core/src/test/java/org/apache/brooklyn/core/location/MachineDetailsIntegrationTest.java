5962374329b305bd21a026a9fae1aac83ce8ca47
Aled Sage <aled.sage@gmail.com>
2017-04-18 10:03:13 +0100

    Renamed to MachineDetailsIntegrationTest
    (cherry picked from commit ee0cabde0e2da32a5ba60c2312432baa3e847b25)

--- core/src/test/java/org/apache/brooklyn/core/location/MachineDetailsIntegrationTest.java
+++ core/src/test/java/org/apache/brooklyn/core/location/MachineDetailsIntegrationTest.java
@@ -39,9 +39,9 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class MachineDetailsTest {
+public class MachineDetailsIntegrationTest {
 
-    private static final Logger LOG = LoggerFactory.getLogger(MachineDetailsTest.class);
+    private static final Logger LOG = LoggerFactory.getLogger(MachineDetailsIntegrationTest.class);
 
     TestApplication app;
     ManagementContext mgmt;
