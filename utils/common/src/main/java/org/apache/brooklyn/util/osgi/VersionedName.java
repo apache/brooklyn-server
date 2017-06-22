89fb13955eef518f53e3c3330a20191c89a3cf2e
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2017-06-22 13:17:32 +0100

    record underlying supplied version in VersionedName
    support for persistence, and test

--- utils/common/src/main/java/org/apache/brooklyn/util/osgi/VersionedName.java
+++ utils/common/src/main/java/org/apache/brooklyn/util/osgi/VersionedName.java
@@ -19,52 +19,93 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 import javax.annotation.Nullable;
 
-import com.google.common.base.Objects;
-import org.apache.brooklyn.util.text.Strings;
+import org.apache.brooklyn.util.text.BrooklynVersionSyntax;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.Version;
 
-/**
- * Versioned name of an OSGi bundle.
- * @author Ciprian Ciubotariu <cheepeero@gmx.net>
- */
+import com.google.common.base.Objects;
+
+/** Records a name (string) and version (string),
+ * with conveniences for pretty-printing and converting to OSGi format. */
 public class VersionedName {
-    private final String symbolicName;
-    private final Version version;
+    private final String name;
+    private final String v;
+    
+    @Deprecated // since 0.12.0 - remove along with version and readResolve in 0.13.0
+    private final String symbolicName = null;
+    @Deprecated // since 0.12.0
+    private final Version version = null;
+    
 
+    private Object readResolve() {
+        if (symbolicName!=null || version!=null) {
+            // remove legacy fields, and convert to brooklyn recommended version in the process
+            // (might be slightly weird if bundles are persisted, but code will forgive that,
+            // and if types were persisted this will do the right thing)
+            return new VersionedName(symbolicName, BrooklynVersionSyntax.toGoodBrooklynVersion( version.toString() ));
+        }
+        return this;
+    }
+    
     public VersionedName(Bundle b) {
-        this(b.getSymbolicName(), b.getVersion());
+        this(b.getSymbolicName(), b.getVersion().toString());
     }
 
-    public VersionedName(String symbolicName, @Nullable Version version) {
-        this.symbolicName = checkNotNull(symbolicName, "symbolicName");
-        this.version = version;
+    public VersionedName(String name, String v) {
+        this.name = checkNotNull(name, "name");
+        this.v = v;
+    }
+    public VersionedName(String name, @Nullable Version v) {
+        this.name = checkNotNull(name, "name").toString();
+        this.v = v==null ? null : v.toString();
     }
 
+    
     @Override
     public String toString() {
-        return symbolicName + ":" + Strings.toString(version);
+        return name + ":" + v;
+    }
+    
+    public String toOsgiString() {
+        return name + ":" + getOsgiVersion();
     }
 
     public boolean equals(String sn, String v) {
-        return symbolicName.equals(sn) && (version == null && v == null || version != null && version.toString().equals(v));
+        return name.equals(sn) && Objects.equal(this.v, v);
+        // could also consider equal if the osgi mapping is equal (but for now we don't; if caller wants that, they should create Version)
+        // || (v!=null && Version.parseVersion(BrooklynVersionSyntax.toValidOsgiVersion(v)).equals(getOsgiVersion())));
     }
 
     public boolean equals(String sn, Version v) {
-        return symbolicName.equals(sn) && (version == null && v == null || version != null && version.equals(v));
+        return name.equals(sn) && Objects.equal(getOsgiVersion(), v);
     }
 
     public String getSymbolicName() {
-        return symbolicName;
+        return name;
     }
 
+    private transient Version cachedOsgiVersion;
+    @Nullable
+    public Version getOsgiVersion() {
+        if (cachedOsgiVersion==null && v!=null) {
+            cachedOsgiVersion = v==null ? null : Version.parseVersion(BrooklynVersionSyntax.toValidOsgiVersion(v));
+        }
+        return cachedOsgiVersion;
+    }
+
+    @Nullable
+    public String getVersionString() {
+        return v;
+    }
+    
+    @Deprecated /** @deprecated since 0.12.0 use {@link #getVersionString()} or {@link #getOsgiVersion()} */
     public Version getVersion() {
-        return version;
+        return getOsgiVersion();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(symbolicName, version);
+        return Objects.hashCode(name, v);
     }
 
     @Override
@@ -73,14 +114,23 @@ public class VersionedName {
             return false;
         }
         VersionedName o = (VersionedName) other;
-        return Objects.equal(symbolicName, o.symbolicName) && Objects.equal(version, o.version);
+        return Objects.equal(name, o.name) && Objects.equal(v, o.v);
+    }
+    
+    /** As {@link #equals(Object)} but accepting the argument as equal if versions are identical when injected to OSGi-valid versions */
+    public boolean equalsOsgi(Object other) {
+        if (!(other instanceof VersionedName)) {
+            return false;
+        }
+        VersionedName o = (VersionedName) other;
+        return Objects.equal(name, o.name) && Objects.equal(getOsgiVersion(), o.getOsgiVersion());        
     }
 
     public static VersionedName fromString(String nameOptionalColonVersion) {
         if (nameOptionalColonVersion==null) return null;
         int colon = nameOptionalColonVersion.indexOf(':');
         if (colon<0) throw new IllegalArgumentException("Versioned name '"+nameOptionalColonVersion+"' must be of form 'name:version'");
-        return new VersionedName(nameOptionalColonVersion.substring(0, colon), Version.parseVersion(nameOptionalColonVersion.substring(colon+1)));
+        return new VersionedName(nameOptionalColonVersion.substring(0, colon), nameOptionalColonVersion.substring(colon+1));
     }
 
 }
