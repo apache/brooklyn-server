b7247784dc3854bf873d41d41bb4a730e65dfc0d
Aled Sage <aled.sage@gmail.com>
2017-07-04 07:07:05 +0100

    Adds AsyncStartable marker interface

---
+++ core/src/main/java/org/apache/brooklyn/core/entity/trait/AsyncStartable.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.brooklyn.core.entity.trait;
+
+import com.google.common.annotations.Beta;
+
+/**
+ * Marker interface, indicating that this starts up asynchronously.
+ * 
+ * That is, calling {@link Startable#start(java.util.Collection)} can return while the entity is 
+ * still in the state {@link org.apache.brooklyn.core.entity.lifecycle.Lifecycle#STARTING}. It is 
+ * expected that the entity will subsequently transition to 
+ * {@link org.apache.brooklyn.core.entity.lifecycle.Lifecycle#RUNNING} 
+ * (and {@link org.apache.brooklyn.core.entity.Attributes#SERVICE} {@code true}).
+ */
+@Beta
+public interface AsyncStartable extends Startable {
+}
