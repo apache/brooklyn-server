635b0c276f137005268dc832c6801bf950a87c21
Sam Corbett <sam.corbett@cloudsoftcorp.com>
2017-07-28 12:49:00 +0100

    Restore HTTP annotations to deprecated API methods
    The missing annotations meant that brooklyn-client was unable to proxy
    the affected classes (since Resteasy expects to work with every method).
    I think the simplest way to solve this is to expose the deprecated
    methods at .../deprecated.

--- rest/rest-api/src/main/java/org/apache/brooklyn/rest/api/ActivityApi.java
+++ rest/rest-api/src/main/java/org/apache/brooklyn/rest/api/ActivityApi.java
@@ -72,10 +72,16 @@ public interface ActivityApi {
             @QueryParam("limit") @DefaultValue("200") int limit,
             @ApiParam(value = "Max depth to traverse, or -1 for all (default)", required = false) 
             @QueryParam("maxDepth") @DefaultValue("-1") int maxDepth);
-    
+
     /** @deprecated since 0.12.0 use {@link #getAllChildrenAsMap(String, int, int)} with depth -1 */
+    @GET
+    @Path("/{task}/children/recurse/deprecated")
+    @ApiOperation(
+            value = "Fetch all child tasks details as Map<String,TaskSummary> map key == Task ID",
+            response = Map.class)
     @Deprecated
-    public Map<String,TaskSummary> getAllChildrenAsMap(String taskId);
+    public Map<String,TaskSummary> getAllChildrenAsMap(
+            @ApiParam(value = "Task ID", required = true) @PathParam("task") String taskId);
 
     @GET
     @Path("/{task}/stream/{streamId}")
