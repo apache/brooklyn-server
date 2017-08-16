d87bddc215d95046ba4b532578481bada359e1f9
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2017-08-16 08:40:10 +0100

    add test which fails replicating the problem

--- camp/camp-brooklyn/src/test/java/org/apache/brooklyn/camp/brooklyn/catalog/CatalogYamlAppTest.java
+++ camp/camp-brooklyn/src/test/java/org/apache/brooklyn/camp/brooklyn/catalog/CatalogYamlAppTest.java
@@ -103,5 +103,10 @@ public class CatalogYamlAppTest extends AbstractYamlTest {
             deleteCatalogEntity("org.apache.brooklyn.entity.stock.BasicApplication");
         }
     }
-    
+
+    @Test
+    public void testAddTemplateForwardReferenceToEntity() throws Exception {
+        addCatalogItems(loadYaml("template-and-app.bom"));
+        createAndStartApplication("services: [ { type: my_app_template } ]");
+    }
 }
