80d7d1e25ec5aa37065929081a70dbe23b088a8d
Aled Sage <aled.sage@gmail.com>
2018-04-20 11:56:24 +0100

    Fix non-deterministic EffectorResourceTest

--- rest/rest-resources/src/test/java/org/apache/brooklyn/rest/resources/EffectorResourceTest.java
+++ rest/rest-resources/src/test/java/org/apache/brooklyn/rest/resources/EffectorResourceTest.java
@@ -27,12 +27,13 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.apache.brooklyn.api.entity.EntitySpec;
+import org.apache.brooklyn.core.entity.Entities;
 import org.apache.brooklyn.core.test.entity.TestEntity;
 import org.apache.brooklyn.entity.stock.BasicApplication;
+import org.apache.brooklyn.rest.api.EffectorApi;
 import org.apache.brooklyn.rest.testing.BrooklynRestResourceTest;
 import org.apache.brooklyn.test.Asserts;
 import org.apache.brooklyn.util.time.Duration;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.google.common.base.Stopwatch;
@@ -50,11 +51,21 @@ public class EffectorResourceTest extends BrooklynRestResourceTest {
     BasicApplication app;
     TestEntity entity;
 
-    @BeforeMethod(alwaysRun = true)
-    public void setUpMethod() throws Exception {
+    @Override
+    public void initMethod() throws Exception {
+        super.initMethod();
         app = getManagementContext().getEntityManager().createEntity(EntitySpec.create(BasicApplication.class)
                 .child(EntitySpec.create(TestEntity.class)));
         entity = (TestEntity) Iterables.getOnlyElement(app.getChildren());
+    }
+
+    @Override
+    public void destroyMethod() throws Exception {
+        try {
+            if (app != null) Entities.destroy(app);
+        } finally {
+            super.destroyMethod();
+        }
     }
 
     @Test
@@ -114,6 +125,18 @@ public class EffectorResourceTest extends BrooklynRestResourceTest {
     
     @Test
     public void testInvokeEffectorWithTimeoutTimesOut() throws Exception {
+        /*
+         * The effector is invoked via:
+         *   Task<?> task = entity.invoke(effector.get(), parameters)
+         *   task.get(timeout)
+         * On timeout, the task is not cancelled. The effector keeps executing in the background.
+         * 
+         * The task might not yet have reached the effector body's sleep. It sometimes is
+         * still setting up the call (e.g. preparing the effector args).
+         * 
+         * The response returned is a snapshot of the task's state/stacktrace at the time
+         * of the timeout.
+         */
         String path = "/applications/"+app.getId()+"/entities/"+entity.getId()+"/effectors/"+"sleepEffector";
 
         Stopwatch stopwatch = Stopwatch.createStarted();
@@ -126,12 +149,16 @@ public class EffectorResourceTest extends BrooklynRestResourceTest {
         assertEquals(response.getStatus(), 202);
         
         String responseBody = response.readEntity(String.class);
-        assertTrue(entity.getCallHistory().contains("sleepEffector"));
+        Asserts.succeedsEventually(() -> assertTrue(entity.getCallHistory().contains("sleepEffector")));
         assertTrue(runDuration.isShorterThan(Asserts.DEFAULT_LONG_TIMEOUT), "runDuration="+runDuration);
         
         // Expect to get a task back, representing the currently executing effector
         Map<?,?> responseMap = new Gson().fromJson(responseBody, Map.class);
         assertTrue((""+responseMap.get("displayName")).contains("sleepEffector"), "responseMap="+responseMap);
-        assertTrue((""+responseMap.get("detailedStatus")).contains("In progress, thread waiting"), "responseMap="+responseMap);
+        
+        String detailedStatus = ""+responseMap.get("detailedStatus");
+        boolean taskSleeping = detailedStatus.contains("In progress, thread waiting") && detailedStatus.contains("TestEntityImpl.sleepEffector");
+        boolean taskPreparing = detailedStatus.contains("In progress (RUNNABLE)") && detailedStatus.contains("EffectorUtils.invokeMethodEffector");
+        assertTrue(taskSleeping || taskPreparing, "responseMap="+responseMap);
     }
 }
