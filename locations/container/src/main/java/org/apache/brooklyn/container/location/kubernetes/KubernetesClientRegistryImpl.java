063aafdc4a90eea9eba78a316e6be2c86ed99176
iuliana <iuliana@cloudsoft.io>
2020-04-06 20:03:20 +0100

    Bumped up Kubernetes-ckient and Kubernetes-model to 4.9.0

--- locations/container/src/main/java/org/apache/brooklyn/container/location/kubernetes/KubernetesClientRegistryImpl.java
+++ locations/container/src/main/java/org/apache/brooklyn/container/location/kubernetes/KubernetesClientRegistryImpl.java
@@ -27,6 +27,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import io.fabric8.kubernetes.client.Config;
 import org.apache.brooklyn.util.core.config.ConfigBag;
 import org.apache.brooklyn.util.exceptions.Exceptions;
 import org.apache.brooklyn.util.text.Strings;
@@ -34,22 +35,12 @@ import org.apache.brooklyn.util.time.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Optional;
 import com.google.common.base.Throwables;
-import com.google.common.collect.Iterables;
 import com.google.common.io.BaseEncoding;
 
-import io.fabric8.kubernetes.api.model.AuthInfo;
-import io.fabric8.kubernetes.api.model.Cluster;
-import io.fabric8.kubernetes.api.model.Config;
-import io.fabric8.kubernetes.api.model.Context;
-import io.fabric8.kubernetes.api.model.NamedAuthInfo;
-import io.fabric8.kubernetes.api.model.NamedCluster;
-import io.fabric8.kubernetes.api.model.NamedContext;
 import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.internal.KubeConfigUtils;
 
 public class KubernetesClientRegistryImpl implements KubernetesClientRegistry {
 
@@ -59,61 +50,20 @@ public class KubernetesClientRegistryImpl implements KubernetesClientRegistry {
 
     @Override
     public KubernetesClient getKubernetesClient(ConfigBag conf) {
-        ConfigBuilder configBuilder = new ConfigBuilder();
-
+        ConfigBuilder configBuilder = null;
         String configFile = conf.get(KubernetesLocationConfig.KUBECONFIG);
+
         if (Strings.isNonBlank(configFile)) {
             try {
                 Path configPath = Paths.get(configFile);
-                Path configFolder = configPath.normalize().getParent();
-                Config kubeconfig = KubeConfigUtils.parseConfig(configPath.toFile());
-                String currentContext = Optional.fromNullable(conf.get(KubernetesLocationConfig.KUBECONFIG_CONTEXT)).or(kubeconfig.getCurrentContext());
-                Optional<NamedContext> foundContext = Iterables.tryFind(kubeconfig.getContexts(), c -> c.getName().equals(currentContext));
-                if (!foundContext.isPresent()) {
-                    throw new IllegalStateException(String.format("Context %s not found", currentContext));
-                }
-                Context context = foundContext.get().getContext();
-                LOG.debug("Context {} additional properties: {}", currentContext, context.getAdditionalProperties());
-                configBuilder.withNamespace(context.getNamespace());
-
-                String user = context.getUser();
-                Optional<NamedAuthInfo> foundAuthInfo = Iterables.tryFind(kubeconfig.getUsers(), u -> u.getName().equals(user));
-                if (!foundAuthInfo.isPresent()) {
-                    throw new IllegalStateException(String.format("Auth info %s not found", user));
-                }
-                AuthInfo auth = foundAuthInfo.get().getUser();
-                LOG.debug("Auth info {} additional properties: {}", user, auth.getAdditionalProperties());
-                configBuilder.withUsername(auth.getUsername());
-                configBuilder.withPassword(auth.getPassword());
-                if (auth.getToken() == null) {
-                    if (auth.getAuthProvider() != null) {
-                        configBuilder.withOauthToken(auth.getAuthProvider().getConfig().get("id-token"));
-                    }
-                } else {
-                    configBuilder.withOauthToken(auth.getToken());
-                }
-                configBuilder.withClientCertFile(getRelativeFile(auth.getClientCertificate(), configFolder));
-                configBuilder.withClientCertData(auth.getClientCertificateData());
-                configBuilder.withClientKeyFile(getRelativeFile(auth.getClientKey(), configFolder));
-                configBuilder.withClientKeyData(auth.getClientKeyData());
-
-                String clusterName = context.getCluster();
-                Optional<NamedCluster> foundCluster = Iterables.tryFind(kubeconfig.getClusters(), c -> c.getName().equals(clusterName));
-                if (!foundCluster.isPresent()) {
-                    throw new IllegalStateException(String.format("Cluster %s not found", clusterName));
-                }
-                Cluster cluster = foundCluster.get().getCluster();
-                configBuilder.withMasterUrl(cluster.getServer());
-                configBuilder.withCaCertFile(getRelativeFile(cluster.getCertificateAuthority(), configFolder));
-                configBuilder.withCaCertData(cluster.getCertificateAuthorityData());
-                configBuilder.withApiVersion(Optional.fromNullable(cluster.getApiVersion()).or("v1"));
-                configBuilder.withTrustCerts(Boolean.TRUE.equals(cluster.getInsecureSkipTlsVerify()));
-                LOG.debug("Cluster {} server: {}", clusterName, cluster.getServer());
-                LOG.debug("Cluster {} additional properties: {}", clusterName, cluster.getAdditionalProperties());
+                Config clientConfig = Config.fromKubeconfig(new String(Files.readAllBytes(configPath)));
+                configBuilder = new ConfigBuilder(clientConfig);
+                LOG.debug("Master URL: {}", clientConfig.getMasterUrl());
             } catch (IOException e) {
                 Exceptions.propagate(e);
             }
         } else {
+            configBuilder = new ConfigBuilder();
             String masterUrl = checkNotNull(conf.get(KubernetesLocationConfig.MASTER_URL), "master url must not be null");
             Boolean trustCerts = conf.get(KubernetesLocationConfig.TRUST_CERTS);
 
@@ -124,8 +74,7 @@ public class KubernetesClientRegistryImpl implements KubernetesClientRegistry {
                 throw Throwables.propagate(e);
             }
 
-            configBuilder.withMasterUrl(masterUrl)
-                    .withTrustCerts(trustCerts != null ? trustCerts : Boolean.FALSE);
+            configBuilder.withMasterUrl(masterUrl).withTrustCerts(trustCerts != null ? trustCerts : Boolean.FALSE);
 
             if (url.getProtocol().equals("https")) {
                 KubernetesCerts certs = new KubernetesCerts(conf);
