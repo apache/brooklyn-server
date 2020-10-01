8ead72f3d5b00fb6545c6c5eb785ecb3632481c2
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2020-10-01 23:25:34 +0100

    add JAR with bean and failing test case
    the problem is that we aren't passing the loader to the bean deserializer,
    so it isn't looking in the right osgi bundles

--- camp/camp-brooklyn/src/test/java/org/apache/brooklyn/camp/brooklyn/CustomTypeConfigYamlOsgiTest.java
+++ camp/camp-brooklyn/src/test/java/org/apache/brooklyn/camp/brooklyn/CustomTypeConfigYamlOsgiTest.java
@@ -25,6 +25,8 @@ import org.apache.brooklyn.api.typereg.RegisteredType;
 import org.apache.brooklyn.config.ConfigKey;
 import org.apache.brooklyn.core.config.ConfigKeys;
 import org.apache.brooklyn.core.entity.Dumper;
+import org.apache.brooklyn.core.mgmt.ha.OsgiBundleInstallationResult;
+import org.apache.brooklyn.core.mgmt.internal.ManagementContextInternal;
 import org.apache.brooklyn.core.resolve.jackson.BeanWithTypePlanTransformer;
 import org.apache.brooklyn.core.test.entity.TestEntity;
 import org.apache.brooklyn.core.typereg.BasicBrooklynTypeRegistry;
@@ -32,6 +34,9 @@ import org.apache.brooklyn.core.typereg.BasicTypeImplementationPlan;
 import org.apache.brooklyn.core.typereg.JavaClassNameTypePlanTransformer;
 import org.apache.brooklyn.core.typereg.RegisteredTypes;
 import org.apache.brooklyn.test.Asserts;
+import org.apache.brooklyn.util.core.ResourceUtils;
+import org.apache.brooklyn.util.exceptions.ReferenceWithError;
+import org.apache.brooklyn.util.osgi.OsgiTestResources;
 import org.apache.brooklyn.util.text.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,5 +50,22 @@ public class CustomTypeConfigYamlOsgiTest extends CustomTypeConfigYamlTest {
     @Override
     protected boolean disableOsgi() {
         return false;
+    }
+
+    @Test
+    public void testLoadBundleBom() throws NoSuchFieldException, IllegalAccessException {
+        ReferenceWithError<OsgiBundleInstallationResult> result = ((ManagementContextInternal) mgmt()).getOsgiManager().get().install(
+                new ResourceUtils(getClass()).getResourceFromUrl(OsgiTestResources.BROOKLYN_TEST_OSGI_BEANS_URL));
+
+        OsgiBundleInstallationResult r = result.getWithError();
+        RegisteredType rt = r.getTypesInstalled().stream().filter(rti -> "sampleBean".equals(rti.getId())).findAny()
+                .orElseThrow(() -> {
+                    throw Asserts.fail("Bean not found; RTs were: " + r.getTypesInstalled());
+                });
+        Asserts.assertEquals(rt.getKind(), RegisteredTypeKind.BEAN);
+
+        Object b1 = mgmt().getTypeRegistry().create(rt, null, null);
+        Object b1n = b1.getClass().getField("number").get(b1);
+        Asserts.assertEquals(b1n, 1);
     }
 }
