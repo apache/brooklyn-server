22cfb5f1bf67c549be8d69d1f4f7392a5bbc7a65
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2020-12-07 13:32:42 +0000

    address PR comments

--- core/src/main/java/org/apache/brooklyn/core/location/MachineLifecycleUtils.java
+++ core/src/main/java/org/apache/brooklyn/core/location/MachineLifecycleUtils.java
@@ -261,7 +261,7 @@ public class MachineLifecycleUtils {
                     primarilySame = false;
                 }
             } else {
-                return false;
+                primarilySame = false;
             }
         }
 
