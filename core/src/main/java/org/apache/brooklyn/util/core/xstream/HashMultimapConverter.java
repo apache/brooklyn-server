8db92924ead78dfd0153e373c48b881d72450ee1
Juan Cabrerizo <juan@cloudsoft.io>
2021-01-14 17:03:21 +0000

    WIP conveter + tests, not Aleds'

--- core/src/main/java/org/apache/brooklyn/util/core/xstream/HashMultimapConverter.java
+++ core/src/main/java/org/apache/brooklyn/util/core/xstream/HashMultimapConverter.java
@@ -19,25 +19,16 @@
 package org.apache.brooklyn.util.core.xstream;
 
 import com.google.common.collect.HashMultimap;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Multimap;
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.converters.Converter;
 import com.thoughtworks.xstream.converters.MarshallingContext;
-import com.thoughtworks.xstream.converters.SingleValueConverter;
 import com.thoughtworks.xstream.converters.UnmarshallingContext;
 import com.thoughtworks.xstream.converters.collections.CollectionConverter;
 import com.thoughtworks.xstream.io.HierarchicalStreamReader;
 import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 import com.thoughtworks.xstream.mapper.Mapper;
+import org.apache.commons.lang3.StringUtils;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.Collection;
-import java.util.Map;
-//import org.apache.brooklyn.util.javalang.ReflectionsTest.K;
+import java.util.Set;
 
 /** serialization of HashMultimap changed after Guava 18;
  * to support pre-Guava-18 serialized objects we just continue to use the old logic.
@@ -45,124 +36,81 @@ import java.util.Map;
  */
 public class HashMultimapConverter extends CollectionConverter{
 
-    private final Mapper mapper;
-
     public HashMultimapConverter(Mapper mapper) {
         super(mapper);
-        this.mapper = mapper;
     }
 
     // TODO how do we convert this from ObjectOutputStream to XStream mapper ?
     // (code below copied from HashMultimap.readObject / writeObject)
-
-
     @Override
     public void marshal(Object source, HierarchicalStreamWriter writer, MarshallingContext context) {
         HashMultimap hashMultimap = (HashMultimap) source;
-//        writeCompleteItem(source, context,writer);
-        XStream xstream = new XStream();
-        try {
-            ObjectOutputStream stream = xstream.createObjectOutputStream(writer);
-//            stream.writeObject(hashMultimap);
-            stream.writeInt(2);
-            writeMultimap(hashMultimap, writer);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
+        writer.addAttribute("serialization", "custom");
+        writer.startNode("unserializable-parents");
+        writer.endNode();
+        writer.startNode(HashMultimap.class.getCanonicalName());
+        writer.startNode("default");
+        writer.endNode();
+        writer.startNode("int");
+        writer.setValue("2"); // HashMultimap.DEFAULT_VALUES_PER_KEY
+        writer.endNode();
+        writer.startNode("int");
+        Set keys = hashMultimap.keys().elementSet();
+        int distinctKeys = hashMultimap.asMap().size();
+        writer.setValue(Integer.toString(distinctKeys));
+        writer.endNode();
+
+        keys.forEach(key -> {
+            writeCompleteItem(key, context, writer);
+            Collection entry = hashMultimap.get(key);
+            writer.startNode("int");
+            writer.setValue(Integer.toString(entry.size()));
+            writer.endNode();
+            entry.forEach(child -> writeCompleteItem(child, context, writer));
+        });
+
+        writer.endNode();
     }
 
     @Override
     public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
         reader.moveDown();
-        if(reader.getNodeName().equals("unserializable-parents")){
+        if (reader.getNodeName().equals("unserializable-parents")) {
             reader.moveUp();
             reader.moveDown();
         }
-        if(reader.getNodeName().equals("com.google.guava:com.google.common.collect.HashMultimap")){
+        if (reader.getNodeName().equals("com.google.common.collect.HashMultimap") || reader.getNodeName().equals("com.google.guava:com.google.common.collect.HashMultimap")) {
             reader.moveDown();
         }
-        if(reader.getNodeName().equals("default")){
+
+        if (reader.getNodeName().equals("default")) {
             reader.moveUp();
             reader.moveDown();
         }
+        reader.moveUp();
+        reader.moveDown();
         String value = reader.getValue();
-
         int distinctKeys = Integer.parseInt(value);
+        reader.moveUp();
+
         HashMultimap<Object, Object> objectObjectHashMultimap = HashMultimap.create();
-
-        XStream xstream = new XStream();
-        try (ObjectInputStream objectInputStream = xstream.createObjectInputStream(reader)) {
-//            objectInputStream.defaultReadObject();
-            populateMultimap(objectObjectHashMultimap, reader, distinctKeys, context);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+        for (int i = 0; i < distinctKeys; i++) {
+            Object key = readCompleteItem(reader, context, null);
+            reader.moveDown();
+            int children= Integer.parseInt(reader.getValue());
+            reader.moveUp();
+            for (int j = 0; j < children; j++) {
+                Object child = readCompleteItem(reader, context, null);
+                objectObjectHashMultimap.put(key, child);
+            }
         }
-        return objectObjectHashMultimap;
-//        int expectedValuesPerKey = stream.readInt();
-//        stream.defaultReadObject();
-//        int distinctKeys = Serialization.readCount(stream);
-//        Map<K, Collection<V>> map = Maps.newHashMapWithExpectedSize(distinctKeys);
-//        setMap(map);
-//        HashMultimapConverter.populateMultimap(this, stream, expectedValuesPerKey);
 
+        return objectObjectHashMultimap;
     }
 
     @Override
     public boolean canConvert(Class type) {
         return HashMultimap.class.isAssignableFrom(type);
-    }
-
-
-    /**
-     * Stores the contents of a multimap in an output stream, as part of serialization. It does not
-     * support concurrent multimaps whose content may change while the method is running. The {@link
-     * Multimap#asMap} view determines the ordering in which data is written to the stream.
-     *
-     * <p>The serialized output consists of the number of distinct keys, and then for each distinct
-     * key: the key, the number of values for that key, and the key's values.
-     */
-    static <K, V> void writeMultimap(Multimap<K, V> multimap, HierarchicalStreamWriter stream)
-            throws IOException {
-/*        stream.defaultWriteObject();
-        stream.writeInt(multimap.asMap().size());
-        for (Map.Entry<K, Collection<V>> entry : multimap.asMap().entrySet()) {
-            stream.writeObject(entry.getKey());
-            stream.writeInt(entry.getValue().size());
-            for (V value : entry.getValue()) {
-                stream.writeObject(value);
-            }
-        }*/
-    }
-
-
-    <K, V> void populateMultimap(
-            HashMultimap<Object, Object> multimap, HierarchicalStreamReader stream, int distinctKeys, UnmarshallingContext context)
-            throws IOException, ClassNotFoundException {
-        for (int i = 0; i < distinctKeys; i++) {
-            stream.moveUp();
-            stream.moveDown();
-
-            stream.moveUp();
-            stream.moveDown();
-            int valueCount = stream.getAttributeCount();
-            K key = (K) stream.getNodeName();
-            @SuppressWarnings("unchecked") // reading data stored by writeMultimap
-            Collection<V> values = (Collection<V>) multimap.get(key);
-//            populateCollection(stream, context, values );
-
-            while(stream.hasMoreChildren()){
-                stream.moveDown();
-//                @SuppressWarnings("unchecked") // reading data stored by writeMultimap
-                Object bareItem = readBareItem(stream, context, values);
-                values.add((V) bareItem);
-//                V value = (V) stream.getValue();
-//                values.add(value);
-                stream.moveUp();
-            }
-
-        }
     }
 }
--- core/src/test/java/org/apache/brooklyn/util/core/xstream/HashMultimapConverterTest.java
+++ core/src/test/java/org/apache/brooklyn/util/core/xstream/HashMultimapConverterTest.java
@@ -1,5 +1,6 @@
 package org.apache.brooklyn.util.core.xstream;
 
+import com.google.common.base.Joiner;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.ImmutableList;
 import com.thoughtworks.xstream.XStream;
@@ -8,6 +9,7 @@ import org.assertj.core.util.Strings;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.net.Inet4Address;
 import java.net.UnknownHostException;
 
 public class HashMultimapConverterTest extends ConverterTestFixture {
@@ -16,6 +18,7 @@ public class HashMultimapConverterTest extends ConverterTestFixture {
     protected void registerConverters(XStream xstream) {
         super.registerConverters(xstream);
         xstream.registerConverter(new HashMultimapConverter(xstream.getMapper()));
+        xstream.registerConverter(new Inet4AddressConverter());
     }
 
     @Test
@@ -25,6 +28,7 @@ public class HashMultimapConverterTest extends ConverterTestFixture {
                 "  <unserializable-parents/>\n",
                 "  <com.google.common.collect.HashMultimap>\n",
                 "    <default/>\n",
+                "    <int>2</int>\n",
                 "    <int>0</int>\n",
                 "  </com.google.common.collect.HashMultimap>\n",
                 "</com.google.common.collect.HashMultimap>");
@@ -38,15 +42,78 @@ public class HashMultimapConverterTest extends ConverterTestFixture {
                 "  <unserializable-parents/>\n",
                 "  <com.google.common.collect.HashMultimap>\n",
                 "    <default/>\n",
-                "    <int>0</int>\n",
+                "    <int>2</int>\n",
+                "    <int>1</int>\n",
+                "    <int>1</int>\n",
+                "    <int>1</int>\n",
+                "    <string>one</string>\n",
                 "  </com.google.common.collect.HashMultimap>\n",
                 "</com.google.common.collect.HashMultimap>");
+        HashMultimap<Object, Object> hashMultimap = HashMultimap.create();
+        hashMultimap.put(1, "one");
+        assertX(hashMultimap, fmr);
+    }
+
+    @Test
+    public void testHashMultimapMultikey() throws UnknownHostException {
+        String fmr = Joiner.on("\n").join(
+                        "<com.google.common.collect.HashMultimap serialization=\"custom\">",
+        "  <unserializable-parents/>",
+        "  <com.google.common.collect.HashMultimap>",
+        "    <default/>",
+        "    <int>2</int>",
+        "    <int>3</int>",
+        "    <string>one</string>",
+        "    <int>1</int>",
+        "    <string>one</string>",
+        "    <string>two</string>",
+        "    <int>2</int>",
+        "    <string>two.two</string>",
+        "    <string>two</string>",
+        "    <string>three</string>",
+        "    <int>1</int>",
+        "    <string>three</string>",
+        "  </com.google.common.collect.HashMultimap>",
+        "</com.google.common.collect.HashMultimap>");
 
         HashMultimap<Object, Object> hashMultimap = HashMultimap.create();
         hashMultimap.put("one", "one");
         hashMultimap.put("two", "two");
         hashMultimap.put("two", "two.two");
+        hashMultimap.put("three", "three");
         assertX(hashMultimap, fmr);
     }
 
+    @Test
+    public void testLegacyHashMultimap() throws UnknownHostException {
+        HashMultimap<Object, Object> obj = HashMultimap.create();
+        obj.put("myInet4Address", Inet4Address.getByName("1.1.1.1"));
+        obj.put("mystring", "myval1");
+        obj.put("mystring", "myval2");
+        obj.put("myintholder", new XmlSerializerTest.IntegerHolder(123));
+
+        String fmt = Joiner.on("\n").join(
+                "<com.google.common.collect.HashMultimap serialization=\"custom\">",
+                "  <unserializable-parents/>",
+                "  <com.google.common.collect.HashMultimap>",
+                "    <default/>",
+                "    <int>2</int>",
+                "    <int>3</int>",
+                "    <string>myintholder</string>",
+                "    <int>1</int>",
+                "    <org.apache.brooklyn.util.core.xstream.XmlSerializerTest_-IntegerHolder>",
+                "      <val>123</val>",
+                "    </org.apache.brooklyn.util.core.xstream.XmlSerializerTest_-IntegerHolder>",
+                "    <string>myInet4Address</string>",
+                "    <int>1</int>",
+                "    <java.net.Inet4Address>one.one.one.one/1.1.1.1</java.net.Inet4Address>",
+                "    <string>mystring</string>",
+                "    <int>2</int>",
+                "    <string>myval1</string>",
+                "    <string>myval2</string>",
+                "  </com.google.common.collect.HashMultimap>",
+                "</com.google.common.collect.HashMultimap>");
+
+        assertX(obj, fmt);
+    }
 }
