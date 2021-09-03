774e686a08dfc53297fbbb56912bded64a434b1c
Mykola Mandra <mykola.mandra@cloudsoft.io>
2021-09-03 20:46:46 +0100

    Encode stream type in the link as UTF-8
    Signed-off-by: Mykola Mandra <mykola.mandra@cloudsoft.io>

--- rest/rest-resources/src/main/java/org/apache/brooklyn/rest/transform/TaskTransformer.java
+++ rest/rest-resources/src/main/java/org/apache/brooklyn/rest/transform/TaskTransformer.java
@@ -28,6 +28,7 @@ import java.util.Map;
 
 import javax.annotation.Nullable;
 
+import org.apache.brooklyn.util.text.StringEscapes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.brooklyn.api.entity.Entity;
@@ -108,7 +109,7 @@ public class TaskTransformer {
                 metadata.add("size", stream.streamSize.get());
                 metadata.add("sizeText", Strings.makeSizeString(stream.streamSize.get()));
             }
-            String link = selfLink+"/stream/"+stream.streamType;
+            String link = selfLink + "/stream/" + StringEscapes.escapeHtmlFormUrl(stream.streamType).replaceAll("\\+", "%20");
             streams.put(stream.streamType, new LinkWithMetadata(link, metadata));
         }
         
