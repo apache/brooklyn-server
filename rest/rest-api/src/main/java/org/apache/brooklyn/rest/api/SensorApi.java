3a4fd028fc5a40dd41ce392a65a3c0c517b83cf3
Martin Harris <github@nakomis.com>
2021-12-06 17:16:22 +0000

    Adds ApiResponse to SensorApi

--- rest/rest-api/src/main/java/org/apache/brooklyn/rest/api/SensorApi.java
+++ rest/rest-api/src/main/java/org/apache/brooklyn/rest/api/SensorApi.java
@@ -51,7 +51,11 @@ public interface SensorApi {
             response = org.apache.brooklyn.rest.domain.SensorSummary.class,
             responseContainer = "List")
     @ApiResponses(value = {
-            @ApiResponse(code = 404, message = "Could not find application or entity")
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 404, message = "Could not find application or entity"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
     })
     public List<SensorSummary> list(
             @ApiParam(value = "Application ID or name", required = true)
@@ -62,6 +66,13 @@ public interface SensorApi {
     @GET
     @Path("/current-state")
     @ApiOperation(value = "Fetch sensor values in batch", notes="Returns a map of sensor name to value")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 404, message = "Could not find application or entity"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
     public Map<String, Object> batchSensorRead(
             @ApiParam(value = "Application ID or name", required = true)
             @PathParam("application") final String application,
@@ -76,10 +87,14 @@ public interface SensorApi {
     @GET
     @Path("/{sensor}")
     @ApiOperation(value = "Fetch sensor value (json)", response = Object.class)
-    @ApiResponses(value = {
-            @ApiResponse(code = 404, message = "Could not find application, entity or sensor")
-    })
     @Produces({MediaType.APPLICATION_JSON})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 404, message = "Application, entity, or sensor not found"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
     public Object get(
             @ApiParam(value = "Application ID or name", required = true)
             @PathParam("application") final String application,
@@ -98,7 +113,11 @@ public interface SensorApi {
     @Path("/{sensor}")
     @ApiOperation(value = "Fetch sensor value (text/plain)", response = String.class)
     @ApiResponses(value = {
-            @ApiResponse(code = 404, message = "Could not find application, entity or sensor")
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 404, message = "Application, entity, or sensor not found"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
     })
     @Produces(MediaType.TEXT_PLAIN + ";qs=0.9")
     public String getPlain(
@@ -117,7 +136,11 @@ public interface SensorApi {
     @POST
     @ApiOperation(value = "Manually set multiple sensor values")
     @ApiResponses(value = {
-            @ApiResponse(code = 404, message = "Could not find application or entity")
+            @ApiResponse(code = 201, message = "Accepted"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 404, message = "Application, entity, or sensor not found"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
     })
     @SuppressWarnings("rawtypes")
     public void setFromMap(
@@ -132,7 +155,11 @@ public interface SensorApi {
     @Path("/{sensor}")
     @ApiOperation(value = "Manually set a sensor value")
     @ApiResponses(value = {
-            @ApiResponse(code = 404, message = "Could not find application, entity or sensor")
+            @ApiResponse(code = 201, message = "Accepted"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 404, message = "Application, entity, or sensor not found"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
     })
     public void set(
             @ApiParam(value = "Application ID or name", required = true)
@@ -148,7 +175,11 @@ public interface SensorApi {
     @Path("/{sensor}")
     @ApiOperation(value = "Manually clear a sensor value")
     @ApiResponses(value = {
-            @ApiResponse(code = 404, message = "Could not find application, entity or sensor")
+            @ApiResponse(code = 200, message = "Accepted"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 404, message = "Application, entity, or sensor not found"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
     })
     public void delete(
             @ApiParam(value = "Application ID or name", required = true)
