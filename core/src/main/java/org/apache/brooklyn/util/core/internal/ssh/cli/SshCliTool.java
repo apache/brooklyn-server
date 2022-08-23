81c2dafa31e4d549f04ec0db5d81dd7aab689224
Mykola Mandra <mykola.mandra@cloudsoft.io>
2022-08-23 16:17:00 +0100

    SSH and SCP executables to get params via env vars
    This enables configuring a custom SSH adn SCP CLI tools allowing
    processing applying password instead of private key for identification,
    which is not allowed in SshCliTool by default.
    
    Signed-off-by: Mykola Mandra <mykola.mandra@cloudsoft.io>

--- core/src/main/java/org/apache/brooklyn/util/core/internal/ssh/cli/SshCliTool.java
+++ core/src/main/java/org/apache/brooklyn/util/core/internal/ssh/cli/SshCliTool.java
@@ -18,7 +18,18 @@
  */
 package org.apache.brooklyn.util.core.internal.ssh.cli;
 
-import static com.google.common.base.Preconditions.checkNotNull;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import org.apache.brooklyn.config.ConfigKey;
+import org.apache.brooklyn.core.config.ConfigKeys;
+import org.apache.brooklyn.util.collections.MutableMap;
+import org.apache.brooklyn.util.core.internal.ssh.SshAbstractTool;
+import org.apache.brooklyn.util.core.internal.ssh.SshTool;
+import org.apache.brooklyn.util.core.internal.ssh.process.ProcessTool;
+import org.apache.brooklyn.util.text.StringEscapes.BashStringEscapes;
+import org.apache.brooklyn.util.text.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.InputStream;
@@ -26,21 +37,9 @@ import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
-import org.apache.brooklyn.config.ConfigKey;
-import org.apache.brooklyn.core.config.ConfigKeys;
-import org.apache.brooklyn.util.collections.MutableMap;
-import org.apache.brooklyn.util.core.internal.ssh.SshAbstractTool;
-import org.apache.brooklyn.util.core.internal.ssh.SshTool;
-import org.apache.brooklyn.util.core.internal.ssh.cli.SshCliTool;
-import org.apache.brooklyn.util.core.internal.ssh.process.ProcessTool;
-import org.apache.brooklyn.util.text.Strings;
-import org.apache.brooklyn.util.text.StringEscapes.BashStringEscapes;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Lists;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * For ssh and scp commands, delegating to system calls.
@@ -217,67 +216,92 @@ public class SshCliTool extends SshAbstractTool implements SshTool {
     }
 
     private int scpExec(Map<String,?> props, String from, String to) {
-        File tempFile = null;
+        File tempKeyFile = Objects.isNull(privateKeyData) ? null : writeTempFile(privateKeyData);
+
         try {
+
+            final String scpPassword = Strings.isEmpty(password) ? "" : password;
+            final File scpTempKeyFile = Objects.isNull(privateKeyFile) ? tempKeyFile : null;
+
             List<String> cmd = Lists.newArrayList();
             cmd.add(getOptionalVal(props, PROP_SCP_EXECUTABLE, scpExecutable));
+
+            // set batch mode
             cmd.add("-B");
-            if (privateKeyFile != null) {
+
+            if (Objects.nonNull(scpTempKeyFile)) {
                 cmd.add("-i");
-                cmd.add(privateKeyFile.getAbsolutePath());
-            } else if (privateKeyData != null) {
-                tempFile = writeTempFile(privateKeyData);
-                cmd.add("-i");
-                cmd.add(tempFile.getAbsolutePath());
+                cmd.add(scpTempKeyFile.getAbsolutePath());
             }
+
             if (!strictHostKeyChecking) {
                 cmd.add("-o");
                 cmd.add("StrictHostKeyChecking=no");
             }
+
             if (port != 22) {
                 cmd.add("-P");
-                cmd.add(""+port);
+                cmd.add("" + port);
             }
+
             cmd.add(from);
             cmd.add(to);
-            
-            if (LOG.isTraceEnabled()) LOG.trace("Executing with command: {}", cmd);
-            int result = execProcess(props, cmd);
-            
+
+            Map<String, String> env = MutableMap.of();
+            env.put("SCP_TEMP_KEY_FILE", Objects.isNull(scpTempKeyFile) ? "" : scpTempKeyFile.getAbsolutePath());
+            env.put("SCP_PASSWORD", scpPassword);
+            env.put("SCP_FROM", from);
+            env.put("SCP_TO", to);
+
+
+            if (LOG.isTraceEnabled()) LOG.trace("Executing command: {}; with env: {}", cmd, env);
+            int result = execProcess(props, cmd, env);
             if (LOG.isTraceEnabled()) LOG.trace("Executed command: {}; exit code {}", cmd, result);
+
             return result;
 
         } finally {
-            if (tempFile != null) tempFile.delete();
+            if (tempKeyFile != null) tempKeyFile.delete();
         }
     }
-    
+
     private int sshExec(Map<String,?> props, String command) {
-        File tempKeyFile = null;
+        File tempKeyFile = Objects.isNull(privateKeyData) ? null : writeTempFile(privateKeyData);
+
         try {
+
+            final String sshUser = Strings.isEmpty(getUsername()) ? "" : getUsername();
+            final String sshHost = getHostAddress();
+            final String sshPassword = Strings.isEmpty(password) ? "" : password;
+            final File sshKeyFile = Objects.isNull(privateKeyFile) ? tempKeyFile : null;
+
             List<String> cmd = Lists.newArrayList();
             cmd.add(getOptionalVal(props, PROP_SSH_EXECUTABLE, sshExecutable));
-            String propsFlags = getOptionalVal(props, PROP_SSH_FLAGS, sshFlags);
+
+            String propFlags = getOptionalVal(props, PROP_SSH_FLAGS, this.sshFlags);
+            if (Objects.nonNull(propFlags) && propFlags.trim().length() > 0) {
+                cmd.addAll(Arrays.asList(propFlags.trim().split(" ")));
+            }
+
+            // set batch mode
             cmd.add("-o");
             cmd.add("BatchMode=yes");
-            if (propsFlags!=null && propsFlags.trim().length()>0)
-                cmd.addAll(Arrays.asList(propsFlags.trim().split(" ")));
-            if (privateKeyFile != null) {
+
+            if (Objects.nonNull(sshKeyFile)) {
                 cmd.add("-i");
-                cmd.add(privateKeyFile.getAbsolutePath());
-            } else if (privateKeyData != null) {
-                tempKeyFile = writeTempFile(privateKeyData);
-                cmd.add("-i");
-                cmd.add(tempKeyFile.getAbsolutePath());
+                cmd.add(sshKeyFile.getAbsolutePath());
             }
+
             if (!strictHostKeyChecking) {
                 cmd.add("-o");
                 cmd.add("StrictHostKeyChecking=no");
             }
+
             if (port != 22) {
                 cmd.add("-P");
-                cmd.add(""+port);
+                cmd.add("" + port);
             }
+
             if (allocatePTY) {
                 // have to be careful with double -tt as it can leave a shell session active
                 // when done from bash (ie  ssh -tt localhost < /tmp/myscript.sh);
@@ -285,9 +309,10 @@ public class SshCliTool extends SshAbstractTool implements SshTool {
                 // (and note single -t doesn't work _programmatically_ since the input isn't a terminal)
                 cmd.add("-tt");
             }
-            cmd.add((Strings.isEmpty(getUsername()) ? "" : getUsername()+"@")+getHostAddress());
-            
-            cmd.add("bash -c "+BashStringEscapes.wrapBash(command));
+
+            cmd.add((Strings.isEmpty(sshUser) ? "" : sshUser + "@") + sshHost);
+
+            cmd.add("bash -c " + BashStringEscapes.wrapBash(command));
             // previously we tried these approaches:
             //cmd.add("$(<"+tempCmdFile.getAbsolutePath()+")");
             // only pays attention to the first word; the "; echo Executing ..." get treated as arguments
@@ -296,21 +321,28 @@ public class SshCliTool extends SshAbstractTool implements SshTool {
             // only works if command is a single word
             //cmd.add(tempCmdFile.getAbsolutePath());
             // above of course only works if the metafile is copied across
-            
-            if (LOG.isTraceEnabled()) LOG.trace("Executing ssh with command: {} (with {})", command, cmd);
-            int result = execProcess(props, cmd);
-            
+
+            Map<String, String> env = MutableMap.of();
+            env.put("SSH_HOST", sshHost);
+            env.put("SSH_USER", sshUser);
+            env.put("SSH_PASSWORD", sshPassword);
+            env.put("SSH_COMMAND_BODY", command);
+            env.put("SSH_TEMP_KEY_FILE", Objects.isNull(sshKeyFile) ? "" : sshKeyFile.getAbsolutePath());
+
+            if (LOG.isTraceEnabled()) LOG.trace("Executing command: {}; with env: {}", cmd, env);
+            int result = execProcess(props, cmd, env);
             if (LOG.isTraceEnabled()) LOG.trace("Executed command: {}; exit code {}", cmd, result);
+
             return result;
-            
+
         } finally {
             if (tempKeyFile != null) tempKeyFile.delete();
         }
     }
 
-    private int execProcess(Map<String,?> props, List<String> cmdWords) {
+    private int execProcess(Map<String,?> props, List<String> cmdWords, Map<String,?> env) {
         OutputStream out = getOptionalVal(props, PROP_OUT_STREAM);
         OutputStream err = getOptionalVal(props, PROP_ERR_STREAM);
-        return ProcessTool.execSingleProcess(cmdWords, null, (File)null, out, err, this);
+        return ProcessTool.execSingleProcess(cmdWords, env, (File)null, out, err, this);
     }
 }
