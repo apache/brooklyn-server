e244be1ab835c57a051bb8f4c3db764c33460936
Alex Heneveld <alex@cloudsoft.io>
2023-10-06 15:01:04 +0100

    note on non-det failure

--- camp/camp-brooklyn/src/test/java/org/apache/brooklyn/camp/brooklyn/WorkflowYamlTest.java
+++ camp/camp-brooklyn/src/test/java/org/apache/brooklyn/camp/brooklyn/WorkflowYamlTest.java
@@ -1008,6 +1008,9 @@ public class WorkflowYamlTest extends AbstractYamlTest {
     }
 
     @Test
+    // non-deterministic failure seen once:
+    // testInitializerDelay:1031 failed succeeds-eventually, 75 attempts, 30000ms elapsed:
+    // AssertionError: entity=BasicEntityImpl{id=v65dmc7x81}; attribute=Sensor: x (java.lang.Integer) expected [6] but found [3]
     public void testInitializerDelay() throws Exception {
         Entity app = createAndStartApplication(
                 "services:",
